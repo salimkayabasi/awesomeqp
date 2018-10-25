49dbec08ef47cc565f98792adb0e6c7e5be37a4f
Salim KAYABASI <salim.kayabasi@gmail.com>
2018-10-25 15:55:20 +0200

    using date object instead of timestamp

--- src/mq.js
+++ src/mq.js
@@ -59,7 +59,7 @@ module.exports = class MQ extends EventEmitter {
     const message = {
       version,
       name,
-      timestamp: Date.now(),
+      timestamp: new Date(),
       payload: payload || {},
     };
     return Buffer.from(JSON.stringify(message));
